6c0595294b8111ba8b58ce80ddb952f4d721fe8c
Abhishek kumar <abhishekkumargond592@gmail.com>
2024-10-24 11:08:15 +0530

    volume Functionality edite

--- index.js
+++ index.js
@@ -14,30 +14,29 @@ menueOpen.addEventListener("click", function menueOpen() {
   videoInput.click();
 });
 const acceptInput = (obj) => {
-
-  const selectedfile = obj.target.files[0];// for selecting file
-  const link = URL.createObjectURL(selectedfile); // generating Url of selecting File 
+  const selectedfile = obj.target.files[0]; // for selecting file
+  const link = URL.createObjectURL(selectedfile); // generating Url of selecting File
   const video = document.createElement("video");
   video.src = link;
   video.setAttribute("class", "video");
-  videoPlayer.appendChild(video);//Inserting Any Element Into The Html 
+  videoPlayer.appendChild(video); //Inserting Any Element Into The Html
   video.play();
   video.controls = "true";
   video.defaultPlaybackRate = 1;
   video.loop;
 
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.>>>>>>>>>>>
-  //  This Is Not Usable Code Only For Learning 
-  // const currentTime = video.currentTime;// Taking Current Time Of Videos 
+  //  This Is Not Usable Code Only For Learning
+  // const currentTime = video.currentTime;// Taking Current Time Of Videos
 
   // setTimeout(() => {
   //   video.currentTime = 720;
-  //   console.log(video.duration); // given the duration of the videos 
-  // }, 10000); //set a currnent Time of video 
+  //   console.log(video.duration); // given the duration of the videos
+  // }, 10000); //set a currnent Time of video
 
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-}
-videoInput.addEventListener("change", acceptInput); //change are using for 
+};
+videoInput.addEventListener("change", acceptInput); //change are using for
 
 // volume and Speed Code ****************************************
 
@@ -46,24 +45,23 @@ const SpeedUpHandler = () => {
   if (videoElement == null) {
     return;
   }
-  // Property 
+  // Property
   const speedIncrease = videoElement.playbackRate + 0.5;
   videoElement.playbackRate = speedIncrease;
-  shomesage(speedIncrease);
+  shomesage(speedIncrease + "x");
   // console.log(speedIncrease);
-
-}
+};
 const SpeedDownHandler = () => {
   const speedDownElement = document.querySelector("video");
   if (speedDownElement == null) {
     return;
   }
-  // Property 
+  // Property
   const speedDecrease = speedDownElement.playbackRate - 0.5;
   speedDownElement.playbackRate = speedDecrease;
-  shomesage(speedDecrease);
-  console.log("Increase : " + speedDecrease);
-}
+  shomesage(speedDecrease + "x");
+  // console.log("Increase : " + speedDecrease);
+};
 const volueUpHandler = () => {
   const volumeUpElement = document.querySelector("video");
   if (volumeUpElement == null) {
@@ -72,13 +70,13 @@ const volueUpHandler = () => {
 
   const volumeUP = volumeUpElement.volume + 0.2;
   volumeUpElement.volume = volumeUP;
-  if (volumeUpElement.volume >= 1) {
+  if (volumeUpElement.volume > 1) {
     return;
   }
-  const persentage = (volumeUpElement.volume * 100) + "%";//create volume function panding
-  message(persentage);
+  const persentage = Math.floor(volumeUP * 100) + "%"; //create volume function panding
+  shomesage(persentage);
   console.log("Valume Increase...." + volumeUpElement.volume);
-}
+};
 const volueDownHandler = () => {
   const volumeDownElement = document.querySelector("video");
   if (volumeDownElement == null) {
@@ -86,15 +84,16 @@ const volueDownHandler = () => {
   }
   const volumeDown = volumeDownElement.volume - 0.2;
   volumeDownElement.volume = volumeDown;
-  if (volumeDownElement.volume <= 0.1) {
+  if (volumeDownElement.volume < 0.1) {
     return;
   }
-  message((volumeDownElement.volume * 100) + "%");//create volume function panding
+  const persentage1 = Math.floor(volumeDown * 100) + "%";
+  shomesage(persentage1); //create volume function panding
   console.log("Valume Decrease...." + volumeDownElement.volume);
-}
+};
 
 function shomesage(message) {
-  tost.textContent = message + "x";
+  tost.textContent = message;
   tost.style.display = "block";
   setTimeout(() => {
     tost.style.display = "none";
@@ -105,6 +104,3 @@ speedUp.addEventListener("click", SpeedUpHandler);
 speedDown.addEventListener("click", SpeedDownHandler);
 volumeUp.addEventListener("click", volueUpHandler);
 volumeDown.addEventListener("click", volueDownHandler);
-
-
-
